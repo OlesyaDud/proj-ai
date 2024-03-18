8c8614513a917e2e8e7452c76584266d426e2321
lesya <wodadevelopment@gmail.com>
2024-03-18 11:49:33 -0400

    commit

--- proj-ai/src/App.jsx
+++ proj-ai/src/App.jsx
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+import Roadmap from "./components/Roadmap";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
